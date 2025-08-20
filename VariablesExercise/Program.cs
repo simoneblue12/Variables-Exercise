106f79618bff214599ae955f235428f807308320
Simone Mercer <simoneblue12@gmail.com>
2025-08-20 16:07:39 -0700

    Declared and initialized variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Simone";
+            int age = 23;
+            double avg = 92.4;
+            decimal bankAccount = 10000.00m;
+            char initial = 'S';
+            bool isMarried = false;
+
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nAverage: {avg}\nMoney: {bankAccount}\nInitial: {initial}\nMarried: {isMarried}");
         }
     }
 }
